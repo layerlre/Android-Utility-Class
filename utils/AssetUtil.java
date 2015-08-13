28ac3a8de678805a9db2553f096a538d95254abf
LaYer Sutachad <layerlre@gmail.com>
2015-08-13 16:03:44 +0700

    Add AssetUtil

---
+++ utils/AssetUtil.java
@@ -0,0 +1,115 @@
+package com.android.lexikon.utils;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by layer on 13/8/2558.
+ */
+public class AssetUtil {
+
+    public static File copyAssets(Context context,String fileName) {
+        AssetManager assetManager = context.getAssets();
+        File outFile = null;
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(fileName);
+            // copy file path
+            outFile = new File(context.getExternalFilesDir(null), fileName);
+            out = new FileOutputStream(outFile);
+            copyFile(in, out);
+        } catch (IOException e) {
+            Log.e("tag", "Failed to copy asset file: " + fileName, e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    // NOOP
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+
+                } catch (IOException e) {
+                    // NOOP
+                }
+            }
+
+        }
+        return outFile;
+    }
+
+        public static File copyAssets(Context context,String fileName,File outFile) {
+        AssetManager assetManager = context.getAssets();
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(fileName);
+            out = new FileOutputStream(outFile);
+            copyFile(in, out);
+        } catch (IOException e) {
+            Log.e("tag", "Failed to copy asset file: " + fileName, e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    // NOOP
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+
+                } catch (IOException e) {
+                    // NOOP
+                }
+            }
+
+        }
+        return outFile;
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+
+    public static String loadJSONFromAsset(Context context, String filename) {
+        String json = null;
+        try {
+
+            InputStream is = context.getAssets().open(filename);
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
+    }
+}
