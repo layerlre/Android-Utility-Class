9207577ee2e7187609ade7f1ffaf143b1f89e650
LaYer Sutachad <layerlre@gmail.com>
2015-08-13 16:08:26 +0700

    update customview dir

---
+++ utils/custom-view/WrapListView.java
@@ -0,0 +1,29 @@
+package utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by layer on 9/8/2558.
+ */
+public class WrapListView extends ListView{
+    public WrapListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WrapListView(Context context) {
+        super(context);
+    }
+
+    public WrapListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
